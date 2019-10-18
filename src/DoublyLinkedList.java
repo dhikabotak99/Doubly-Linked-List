c69757ca981f8409c8cb6f95beb8b620b10453e0
dhikabotak99 <wicakbotak99@gmail.com>
2019-10-18 19:04:23 +0700

    Make Constructor for DoublyLinkedList

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -6,4 +6,10 @@ public class DoublyLinkedList {
     public Node head;
     public Node tail;
     public int size;
+    
+    public DoublyLinkedList(){
+        this.head=null;
+        this.tail=null;
+        this.size=0;
+    }
 }
